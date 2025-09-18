f9fb16fcdd722165d552ce1ca24e2b2c6b722179
bradm1814 <bradm1814@gmail.com>
2025-09-17 20:56:09 -0400

    completed assignment 2 DSA

--- CSC-575-DSA/Assignments/Assignment 2/Question 1/ChecklistElement.java
+++ CSC-575-DSA/Assignments/Assignment 2/Question 1/ChecklistElement.java
@@ -12,47 +12,28 @@ class CheckListElement{
         lst.add("Papaya");
         lst.add("Mulberry");
         lst.add("Apple");
-        lst.add("Cherry");
-        lst.add("Guava");
+        //lst.add("Cherry");
+        //lst.add("Guava");
 
         //print contents of the populated Linked List
         System.out.println("Initial contents of the Linked List: " + lst);
 
-        //call and print check function on cherry
-        System.out.println(Check(lst, "Cherry"));
+        //check if cherry is present
+        if (lst.contains("Cherry")){
+            System.out.println("Fruit Cherry is present");
+        }else{
+            System.out.println("Fruit Cherry is not present");
+        }
         
-        //check function on guava
-        System.out.println(Check(lst, "Guava"));
+        //check if guava is present
+        if (lst.contains("Guava")){
+            System.out.println("Fruit Guava is present");
+        }else{
+            System.out.println("Fruit Guava is not present");
     }
 
-    //create check function
-    /**
-     * Function Name: Check
-     * 
-     * @param linkedList
-     * @param item
-     * @return
-     * 
-     * In this Function:
-     * 1. take input of a linked list and a string to see if it is in the linked list
-     * 2. while loop the ends if it reaches the tail (.hasNext() returns False)
-     * 3. If statement to check if the input string is inside the linked list and returns a message stating that it is
-     * 4. If the while loop ends without finding a match it return a message saying it in there
-     *
-     */
-    public static String Check(LinkedList<String> linkedList, String item){
-        var iterator = linkedList.iterator();
-        while (iterator.hasNext()){
-            if (iterator.next().toLowerCase().equals(item.toLowerCase())){
-                String message = "Fruit "+ item +" is present";
-                return message;
-            }
-        }
-        String message = "Fruit "+ item +" is not present";
-        return message;
-        }
     }
-
+}
 
 
 
--- CSC-575-DSA/Assignments/Assignment 2/Question 2/ReplaceElement.java
+++ CSC-575-DSA/Assignments/Assignment 2/Question 2/ReplaceElement.java
@@ -1,14 +1,3 @@
-// The aim is to define a class named Replace_Element. 
-// Create a LinkedList object named fru_list  with a type parameter of String, indicating that it will store strings as its elements.
-// Add the Strings elements such as "Watermelon", "Apple", "Cherry", and "Banana" to fru_list .
-// Print the initial contents of fru_list before any modifications.
-// Replace the element at index 2 in fru_list with the string "Pineapple". The set method takes two parameters: 
-// the index at which the element needs to be replaced, and the new value to be inserted at that index.
-// Print the message "Replace an Element in a linked list."
-// Print the contents of fru_list after replacing the element, which will now include "Pineapple" at index 2.
-// The output of the program will be the initial contents of fru_list printed on the first line, 
-// followed by the message "Replace an Element in a linked list." printed on the second line, 
-// and the updated contents of fru_list with "Pineapple" at index 2 printed on the third line.
 
 //import LinkedList module
 import java.util.LinkedList;
